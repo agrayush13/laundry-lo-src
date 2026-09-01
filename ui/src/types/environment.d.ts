dc991945fe6a3dd89072a4cd2593e259410c47c6
agrayush13 <agr.ayush13@gmail.com>
2026-09-01 14:17:45 +0530

    chore(auth): configure Supabase runtime

---
+++ ui/src/types/environment.d.ts
@@ -0,0 +1,3 @@
+/* eslint-disable @typescript-eslint/naming-convention -- webpack compile-time constants */
+declare const __SUPABASE_URL__: string;
+declare const __SUPABASE_PUBLISHABLE_KEY__: string;
